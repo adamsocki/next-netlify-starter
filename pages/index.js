115ba810418d97f86eff790b6923fab0f1eb6c9a
AdamS <65431559+adamsocki@users.noreply.github.com>
2022-07-15 23:53:18 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Aadm Socki - Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to Adam Socki's app!" />
         <p className="description">
-          This is Adam Sockis first Page. <code>pages/index.js</code>
+          This is Adam Sockis first Page. 
         </p>
       </main>
 
